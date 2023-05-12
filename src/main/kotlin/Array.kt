d3b992df35fa6cfa5b8134217b16c2795b62a53b
Sonal <sonal.dhage26@gmail.com>
2023-05-12 20:04:00 +0200

    Array exmaple

---
+++ src/main/kotlin/Array.kt
@@ -0,0 +1,26 @@
+fun main() {
+    var arr = arrayOf("One","Two","Three")
+    var arr1 = arrayOf(1,2,3)
+
+    for (i in arr.withIndex()){
+        println(i)
+    }
+
+    for ((i, e) in arr.withIndex()){ // i = index, e = element
+        println("$i - $e")
+    }
+
+    //ways of printing ana array
+    println(arr[0])
+    println(arr.get(1))
+
+    //Changing the value of an element
+    arr.set(0, "Hello")
+    println(arr.get(0))
+
+    //Array size
+    println("Display the array size:")
+    println(arr.size)
+    println("Accessing array element from 2nd position::")
+    println(arr[2])
+}
